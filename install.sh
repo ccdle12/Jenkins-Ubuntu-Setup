4c8bd4e51a3bc1e4beb0355663d4ecc073dabaa2
ccdle12 <chris.coverdale24@gmail.com>
2019-05-24 10:35:51 +0800

    install: adding update before jenkins and making executable

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ apt update
 apt install openjdk-8-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt update
 apt install jenkins -y
 systemctl status jenkins
