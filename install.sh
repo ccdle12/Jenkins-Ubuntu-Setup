7101143a7a7f621badd823a9875924aafa1e851b
root <root@localhost.localdomain>
2019-05-31 15:54:00 +0800

    install.sh: adding docker install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 apt update
+apt install docker.io -y
 apt install openjdk-8-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
